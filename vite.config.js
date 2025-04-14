6393527afbeab2ee2a818d1c6625e09783c7f141
Bia Ingrid <bia.ingrid2@gmail.com>
2025-04-14 19:26:37 -0300

    remove type module para corrigir deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    fs: {
-      strict: false,
-    },
+    outDir: "dist",           // Diretório de saída para o build
+    chunkSizeWarningLimit: 1000, // Evita warning de tamanho de chunk
   },
 });
